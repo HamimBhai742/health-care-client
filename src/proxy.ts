693a8c86fe1ce7049f03ee7dd38afc2d5b22873c
HamimBhai742 <omagootruelove147@gmail.com>
2026-01-05 09:08:29 +0600

    Update

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,9 +1,35 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+type userRole = 'patient' | 'doctor' | 'admin';
+
+const authRoute=['/register','/login','/forgot-password','/reset-password']
+
+const commonRoute={
+  exact:['/my-profile','/settings'],
+  pattern:[]
+}
+
+const doctorProtectedRoute={
+  exact:[/^\/docotr/],
+  pattern:[]
+}
+
+const patientProtectedRoute={
+  exact:[/^\/patient/],
+  pattern:[]
+}
+
+const adminProtectedRoute={
+  exact:[/^\/admin/],
+  pattern:[]
+}
+
+
 
 // This function can be marked `async` if using `await` inside
 export function proxy(request: NextRequest) {
-  return NextResponse.redirect(new URL('/home', request.url))
+  return NextResponse.redirect(new URL('/home', request.url));
 }
 
 // Alternatively, you can use a default export:
@@ -11,5 +37,8 @@ export function proxy(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: '/about/:path*',
-}
+  matcher: [
+    // Exclude API routes, static files, image optimizations, and .png files
+    '/((?!api|_next/static|_next/image|.*\\.png$).*)',
+  ],
+};
